e9157728acd4a4d5657bb577ec163673faf1bfc5
Grayson Cordell <Grayson.Cordell@vanderbilt.edu>
2025-02-27 19:34:22 -0600

    Update shims.d.ts

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -2,6 +2,16 @@ declare module '*.png' {
   const value: any
   export = value
 }
+
 declare interface Window {
   // extend the window
 }
+
+// with unplugin-vue-markdown, markdown files can be treated as Vue components
+declare module '*.md' {
+  import type { DefineComponent } from 'vue'
+
+  const component: DefineComponent<object, object, any>
+  export default component
+}
+

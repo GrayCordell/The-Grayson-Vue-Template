c4eda3e043776470d4c1a4d5970487febb1f5d88
Grayson Cordell <Grayson.Cordell@vanderibilt.edu>
2025-03-20 13:53:36 -0500

    Update theme.ts

--- src/app/setup/styles/theme.ts
+++ src/app/setup/styles/theme.ts
@@ -30,13 +30,62 @@ const allThemes: AllThemes = {
       },
     },
   },
-  kidsScheme: {
+  mathKidsScheme: {
     condition: () => false, // flags.getIsKids(),
     theme: {
       variables: {
         '--accent': '#7EC8E3',
         '--accent-hover': '#6599cb',
-        '--accent-darker': '#4a7a9b',
+        '--betty': '#d0e6d6',
+        '--mentor': '#6AAFAE',
+        '--accent-darker': 'black',
+        '--canvas-background-color': '#fafafa',
+        '--canvas-pattern-color': '#f2f2f2',
+        '--agent-border-1': ' ',
+        '--agent-border-2': ' ',
+        '--conversation-divider-color': '#dcdcdc',
+        '--message-text-color': '#333333',
+        '--message-text-shadow': 'none',
+        '--agent-start-background-color': '#f2f2f2',
+        '--conversation-pane-background-color': '#f2f2f2',
+        '--show-hide-button-color': '#f9a03f',
+        '--agent-picture-box-icon-color': '#f9a03f',
+        '--agent-picture-box-text-color': '#333333',
+        '--message-data-text-color': '#969696',
+        '--message-data-text-shadow': 'none',
+
+        // Drag and Drop
+        // Colors
+        '--dnd-color-number': '#d32f2f',
+        '--dnd-color-primary-dark': '#2a2a2a',
+        '--dnd-color-white': '#ffffff',
+
+        // Background Colors
+        '--dnd-bg-secondary': '#e1f5fe',
+        '--dnd-bg-special': '#7ec8e3',
+        '--dnd-bg-input': '#b0d3e6',
+        '--dnd-bg-tertiary': '#b3e5fc',
+        '--dnd-bg-highlight': '#b3e5fc',
+
+        // Border Colors
+        '--dnd-border-secondary': '#2196f3',
+        '--dnd-border-input': '#1976d2',
+        '--dnd-border-tool-box': 'rgb(40, 158, 201)',
+        '--dnd-border-work-area': '#7ec8e3',
+        '--dnd-border-chat': '#406ebd59',
+        '--dnd-border-drop-zone': '#2196f3',
+
+        // Shadow Colors
+        '--dnd-shadow-work-area': 'rgba(126, 200, 227, 1)',
+        '--dnd-shadow-default': 'rgba(0, 0, 0, 0.2)',
+        '--dnd-shadow-light': 'rgba(0, 0, 0, 0.1)',
+        // Drag and Drop area. (Some are actually only for default drop zone that is not override)
+        '--dnd-default-drop-zone-border': '#289ec9',
+        '--dnd-default-drop-zone-background': '#7ec8e3',
+        '--dnd-default-drop-zone-highlight': '#a2d1ec',
+        '--dnd-default-drop-zone-highlight-border': '#6baed6',
+        '--dnd-default-drop-zone-text-color': 'var(--primary)',
+        '--dnd-zone-default-highlight-shadow': 'rgba(65, 110, 189, 0.3)',
       },
     },
   },
